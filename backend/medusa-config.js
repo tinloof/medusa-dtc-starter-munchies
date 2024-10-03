58aeab00a3b0c9b13957cf0fe2d67db916f0c379
Amine <mohamedaminediboune@gmail.com>
2024-10-03 12:14:39 +0100

    use MEDUSA_BACKEND_URL

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -1,16 +1,23 @@
-const { loadEnv, defineConfig } = require('@medusajs/framework/utils')
+const { loadEnv, defineConfig } = require("@medusajs/framework/utils");
 
-loadEnv(process.env.NODE_ENV, process.cwd())
+loadEnv(process.env.NODE_ENV, process.cwd());
 
-module.exports = defineConfig({
+export default defineConfig({
   projectConfig: {
+    redisUrl: process.env.REDIS_URL,
     databaseUrl: process.env.DATABASE_URL,
+    databaseLogging: true,
     http: {
       storeCors: process.env.STORE_CORS,
       adminCors: process.env.ADMIN_CORS,
       authCors: process.env.AUTH_CORS,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
-})
+    },
+  },
+  admin: {
+    backendUrl:
+      process.env.MEDUSA_BACKEND_URL ||
+      "https://tricky-assistant-scant.medusajs.app",
+  },
+});
