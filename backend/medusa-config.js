d7ccc13512c7f437947446380d2c8d068d23e5ae
Amine <mohamedaminediboune@gmail.com>
2024-10-04 17:10:35 +0100

    add S3

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -1,4 +1,4 @@
-const { loadEnv, defineConfig } = require("@medusajs/framework/utils");
+const { loadEnv, defineConfig, Modules } = require("@medusajs/framework/utils");
 
 loadEnv(process.env.NODE_ENV, process.cwd());
 
@@ -17,5 +17,26 @@ export default defineConfig({
   },
   admin: {
     backendUrl: "https://munchies.medusajs.app",
+  },
+  modules: {
+    [Modules.FILE]: {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+            },
+          },
+        ],
+      },
+    },
   },
 });
