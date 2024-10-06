efc6739a43dd3227ef9fbeded75946e25cf7c346
Amine <mohamedaminediboune@gmail.com>
2024-10-06 17:46:03 +0100

    options component WIP

--- storefront/app/(website)/products/[handle]/_parts/options.tsx
+++ storefront/app/(website)/products/[handle]/_parts/options.tsx
@@ -1,12 +1,27 @@
+"use client";
+
 import type {StoreProductOption} from "@medusajs/types";
 
 import Select from "@/components/shared/select";
+import {useState} from "react";
 
 type Props = {
   options: StoreProductOption[];
 };
 
 export default function OptionsSelect({options}: Props) {
+  const [selectedOptions, setSelectedOptions] = useState<
+    Record<string, string | undefined>
+  >(() => {
+    return (options || []).reduce(
+      (optionObj, option) => {
+        optionObj[option.id] = undefined;
+        return optionObj;
+      },
+      {} as Record<string, string | undefined>,
+    );
+  });
+
   return options?.map((option) => {
     const values = option.values?.map(({id, value}) => ({
       label: value,
