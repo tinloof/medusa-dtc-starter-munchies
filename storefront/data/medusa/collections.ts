39324a7df0f5ea36077b6ba5992a40bcefe141b6
Amine <mohamedaminediboune@gmail.com>
2024-10-08 13:50:38 +0100

    fix linking content

--- storefront/data/medusa/collections.ts
+++ storefront/data/medusa/collections.ts
@@ -1,12 +1,19 @@
+import type {Collection} from "@/types/sanity.generated";
+import type {StoreCollection} from "@medusajs/types";
+
 import medusa from "./client";
 
 export async function getCollectionByHandle(handle: string) {
   return medusa.store.collection
     .list(
       {
+        fields: "+sanity_collection.*",
         handle,
       },
       {next: {tags: ["collections"]}},
     )
-    .then(({collections}) => collections[0]);
+    .then(
+      ({collections}) =>
+        collections[0] as {sanity_collection: Collection} & StoreCollection,
+    );
 }
--- storefront/data/medusa/products.ts
+++ storefront/data/medusa/products.ts
@@ -1,3 +1,6 @@
+import type {Product} from "@/types/sanity.generated";
+import type {StoreProduct} from "@medusajs/types";
+
 import medusa from "./client";
 
 export async function getProductByHandle(handle: string, region_id: string) {
@@ -5,13 +8,18 @@ export async function getProductByHandle(handle: string, region_id: string) {
     .list(
       {
         fields:
-          "*variants.calculated_price,+variants.inventory_quantity,sanity_product.*",
+          "*variants.calculated_price,+variants.inventory_quantity,+sanity_product.*",
         handle,
         region_id,
       },
       {next: {tags: ["products"]}},
     )
-    .then(({products}) => products[0]);
+    .then(
+      ({products}) =>
+        products[0] as {
+          sanity_product: Product;
+        } & StoreProduct,
+    );
 }
 
 export async function getProductsByIds(ids: string[], region_id: string) {
