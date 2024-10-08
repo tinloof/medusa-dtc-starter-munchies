eccd892397bc1e634d9e4dd10cacef3517e4b40c
Amine <mohamedaminediboune@gmail.com>
2024-10-08 11:55:02 +0100

    fix sync

--- backend/src/api/admin/sanity/syncs/route.ts
+++ backend/src/api/admin/sanity/syncs/route.ts
@@ -1,27 +1,27 @@
-import { MedusaRequest } from "@medusajs/framework"
-import { Modules } from "@medusajs/framework/utils"
-import { IWorkflowEngineService } from "@medusajs/types"
-import { sanityFullSyncWorkflow } from "../../../../workflows/sanity-full-sync"
+import { MedusaRequest } from "@medusajs/framework";
+import { Modules } from "@medusajs/framework/utils";
+import { IWorkflowEngineService } from "@medusajs/types";
+import { sanityFullSyncWorkflow } from "../../../../workflows/sanity-full-sync";
 
 export const POST = async (req, res) => {
   const { transaction } = await sanityFullSyncWorkflow(req.scope).run({
     input: {},
-  })
+  });
 
-  res.json({ transaction_id: transaction.transactionId })
-}
+  res.json({ transaction_id: transaction.transactionId });
+};
 
 export const GET = async (req: MedusaRequest, res) => {
   const wfEngine: IWorkflowEngineService = req.scope.resolve(
-    Modules.WORKFLOW_ENGINE
-  )
+    Modules.WORKFLOW_ENGINE,
+  );
 
   const [executions, count] = await wfEngine.listAndCountWorkflowExecutions(
     {
       workflow_id: sanityFullSyncWorkflow.getName(),
     },
-    { order: { created_at: "DESC" } }
-  )
+    { order: { created_at: "DESC" } },
+  );
 
-  res.json({ workflow_executions: executions, count })
-}
+  res.json({ workflow_executions: executions, count });
+};
