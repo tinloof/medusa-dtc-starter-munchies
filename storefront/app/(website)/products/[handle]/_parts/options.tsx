731b2d774dffd3c001b24aad5d8631ab20d412a9
Amine <mohamedaminediboune@gmail.com>
2024-10-11 16:16:04 +0100

    make variants use search params

--- storefront/app/(website)/products/[handle]/_parts/options.tsx
+++ storefront/app/(website)/products/[handle]/_parts/options.tsx
@@ -14,15 +14,18 @@ export default function OptionsSelect({options}: Props) {
   const {setSelectedOptions} = useProductVariants();
 
   return options?.map((option) => {
-    const values = option.values?.map(({id, value}) => ({
+    const values = option.values?.map(({value}) => ({
       label: value,
-      value: id,
+      value: value.toLowerCase(),
     }));
 
     if (!values || values?.length === 0) return null;
 
     const setOption = (value: string) =>
-      setSelectedOptions((prev) => ({...prev, [option.id]: value}));
+      setSelectedOptions((prev) => ({
+        ...prev,
+        [option.title.toLowerCase()]: value,
+      }));
 
     return <Select key={option.id} options={values} setOption={setOption} />;
   });
