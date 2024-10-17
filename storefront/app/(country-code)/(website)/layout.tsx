c782bedcc1dc4424a56de628d297d1b5b9fb7471
Amine <mohamedaminediboune@gmail.com>
2024-10-17 16:03:04 +0100

    lint

--- storefront/app/(country-code)/(website)/layout.tsx
+++ storefront/app/(country-code)/(website)/layout.tsx
@@ -1,17 +1,11 @@
 import type {Metadata} from "next";
 
-import {ExitPreview} from "@/components/exit-preview";
 import Footer from "@/components/global/footer";
 import Header from "@/components/global/header";
 import PreventBackNavigationSmoothScroll from "@/components/prevent-back-navigation-smooth-scroll";
-import {TailwindIndicator} from "@/components/tailwind-indicator";
 import config from "@/config";
 import {loadGlobalData} from "@/data/sanity";
 import {getOgImages} from "@/data/sanity/resolve-sanity-route-metadata";
-import {VisualEditing} from "next-sanity";
-import {revalidatePath, revalidateTag} from "next/cache";
-import {draftMode} from "next/headers";
-import type React from "react";
 
 export async function generateMetadata(): Promise<Metadata> {
   const data = await loadGlobalData();
@@ -29,8 +23,6 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default async function Layout({children}: {children: React.ReactNode}) {
   const data = await loadGlobalData();
-  const shouldEnableDraftModeToggle =
-    process.env.NODE_ENV === "development" && draftMode().isEnabled;
 
   return (
     <>
@@ -38,33 +30,6 @@ export default async function Layout({children}: {children: React.ReactNode}) {
       {data.header && <Header {...data.header} />}
       <main className="flex-1">{children}</main>
       {data.footer && <Footer {...data.footer} />}
-      {draftMode().isEnabled && (
-        <VisualEditing
-          refresh={async (payload) => {
-            "use server";
-            if (!draftMode().isEnabled) {
-              console.debug(
-                "Skipped manual refresh because draft mode is not enabled",
-              );
-              return;
-            }
-            if (payload.source === "mutation") {
-              if (payload.document.slug?.current) {
-                const tag = `${payload.document._type}:${payload.document.slug.current}`;
-                console.log("Revalidate slug", tag);
-                await revalidateTag(tag);
-              }
-              console.log("Revalidate tag", payload.document._type);
-              return revalidateTag(payload.document._type);
-            }
-            await revalidatePath("/", "layout");
-          }}
-        />
-      )}
-      <TailwindIndicator />
-      {shouldEnableDraftModeToggle && (
-        <ExitPreview enable={draftMode().isEnabled} />
-      )}
     </>
   );
 }
--- storefront/app/(country-code)/layout.tsx
+++ storefront/app/(country-code)/layout.tsx
@@ -1,9 +1,44 @@
 import type {PropsWithChildren} from "react";
 
+import {ExitPreview} from "@/components/exit-preview";
+import {TailwindIndicator} from "@/components/tailwind-indicator";
+import cache from "next/cache";
+import {draftMode} from "next/headers";
+import VisualEditing from "next-sanity/visual-editing/client-component";
+
 export default function Layout({children}: PropsWithChildren) {
+  const shouldEnableDraftModeToggle =
+    process.env.NODE_ENV === "development" && draftMode().isEnabled;
   return (
     <body className="relative flex min-h-screen min-w-min-screen flex-col overflow-x-clip">
       {children}
+      {draftMode().isEnabled && (
+        <VisualEditing
+          refresh={async (payload) => {
+            "use server";
+            if (!draftMode().isEnabled) {
+              console.debug(
+                "Skipped manual refresh because draft mode is not enabled",
+              );
+              return;
+            }
+            if (payload.source === "mutation") {
+              if (payload.document.slug?.current) {
+                const tag = `${payload.document._type}:${payload.document.slug.current}`;
+                console.log("Revalidate slug", tag);
+                await cache.revalidateTag(tag);
+              }
+              console.log("Revalidate tag", payload.document._type);
+              return cache.revalidateTag(payload.document._type);
+            }
+            await cache.revalidatePath("/", "layout");
+          }}
+        />
+      )}
+      <TailwindIndicator />
+      {shouldEnableDraftModeToggle && (
+        <ExitPreview enable={draftMode().isEnabled} />
+      )}
     </body>
   );
 }
