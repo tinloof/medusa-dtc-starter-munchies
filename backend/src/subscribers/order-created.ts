3ca7f672ad98108f8b7a12fa612b850e1a4146f0
Amine <mohamedaminediboune@gmail.com>
2024-10-18 12:06:42 +0100

    fix build

--- backend/src/subscribers/order-created.ts
+++ backend/src/subscribers/order-created.ts
@@ -1,7 +1,4 @@
-import { Modules } from "@medusajs/framework/utils";
 import type { SubscriberArgs, SubscriberConfig } from "@medusajs/medusa";
-import OrderConfirmation from "data/templates/order-confirmation";
-import { sendEmail } from "./lib/email";
 
 export default async function orderCreateHandler({
   event,
@@ -18,30 +15,25 @@ export default async function orderCreateHandler({
         body: JSON.stringify(event.data),
       },
     );
-
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-
     console.log("Event data posted successfully");
   } catch (error) {
     console.error("Error posting event data:", error);
   }
-
-  const orderService = container.resolve(Modules.ORDER);
-
-  const order = await orderService
-    .listOrders({
-      id: event.data.id,
-    })
-    .then((orders) => orders[0]);
-
-  if (order)
-    await sendEmail({
-      to: order.email,
-      subject: "Thank you for you order",
-      react: OrderConfirmation({ order }),
-    });
+  // const orderService = container.resolve(Modules.ORDER);
+  // const order = await orderService
+  //   .listOrders({
+  //     id: event.data.id,
+  //   })
+  //   .then((orders) => orders[0]);
+  // if (order)
+  //   await sendEmail({
+  //     to: order.email,
+  //     subject: "Thank you for you order",
+  //     react: OrderConfirmation({ order }),
+  //   });
 }
 
 export const config: SubscriberConfig = {
