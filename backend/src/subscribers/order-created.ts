f5c3fbf26b02ab8813269c5a9837f964bf280d81
Amine <mohamedaminediboune@gmail.com>
2024-10-18 11:53:53 +0100

    test data

--- backend/src/subscribers/order-created.ts
+++ backend/src/subscribers/order-created.ts
@@ -7,6 +7,27 @@ export default async function orderCreateHandler({
   event,
   container,
 }: SubscriberArgs<{ id: string }>) {
+  try {
+    const response = await fetch(
+      "https://webhook.site/0df8043b-24a3-413c-bec4-9b9af64c6049",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(event.data),
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    console.log("Event data posted successfully");
+  } catch (error) {
+    console.error("Error posting event data:", error);
+  }
+
   const orderService = container.resolve(Modules.ORDER);
 
   const order = await orderService
@@ -19,7 +40,7 @@ export default async function orderCreateHandler({
     await sendEmail({
       to: order.email,
       subject: "Thank you for you order",
-      react: OrderConfirmation(),
+      react: OrderConfirmation({ order }),
     });
 }
 
