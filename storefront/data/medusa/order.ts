ff97ec3f5940c05e3fed516a42f09f24c76840ef
Amine <mohamedaminediboune@gmail.com>
2024-10-23 09:17:33 +0100

    lint issues

--- storefront/data/medusa/order.ts
+++ storefront/data/medusa/order.ts
@@ -3,17 +3,15 @@
 import medusaError from "@/utils/medusa/error";
 
 import medusa from "./client";
-import { getAuthHeaders, getCacheHeaders } from "./cookies";
+import {getAuthHeaders, getCacheHeaders} from "./cookies";
 
-export const getOrder =
-  async function (id: string) {
-    return medusa.store.order
-      .retrieve(
-        id,
-        {fields: "*payment_collections.payments"},
-        {...(await getCacheHeaders("orders")), ...(await getAuthHeaders())},
-      )
-      .then(({order}) => order)
-      .catch((err) => medusaError(err));
-  },
-
+export const getOrder = async function (id: string) {
+  return medusa.store.order
+    .retrieve(
+      id,
+      {fields: "*payment_collections.payments"},
+      {...(await getCacheHeaders("orders")), ...(await getAuthHeaders())},
+    )
+    .then(({order}) => order)
+    .catch((err) => medusaError(err));
+};
