06a2f8f2001b8d12396a92ec3d0db0bd3a50b4ba
Amine <mohamedaminediboune@gmail.com>
2024-10-23 15:54:33 +0100

    fix email styles

--- backend/src/api/store/email/_templates/order-confirmation.tsx
+++ backend/src/api/store/email/_templates/order-confirmation.tsx
@@ -18,12 +18,12 @@ export default function OrderConfirmation({ order }: { order: OrderDTO }) {
 
   return (
     <Layout preview="Order confirmation">
-      <Section className="w-full px-5 mb-12" align="left">
+      <Section className="w-full px-5 mt-5 mb-12" align="left">
         <Img
-          className="max-w-[291px]"
+          className="max-w-[291px] mb-20"
           src="https://cdn.sanity.io/images/1wtf7iqx/production/e04b80a29759293982d74afcde82a169505a3aaa-1166x112.png"
         />
-        <Heading className="mb-3 mt-20" style={title}>
+        <Heading className="pb-3" style={title}>
           Thank you for your order!
         </Heading>
         <EmailBody
