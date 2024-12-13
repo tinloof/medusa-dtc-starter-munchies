36160ba12c5e44171b81b7dd854ae40351a05f68
Ehtisham Afzal <khaj3172@gmail.com>
2024-12-13 07:42:47 +0500

    Changes to be committed: 	modified:   backend/package.json 	modified:   backend/src/api/store/email/_templates/components/footer.tsx 	modified:   backend/yarn.lock 	modified:   storefront/public/favicon-inactive.ico 	modified:   storefront/public/favicon.ico 	new file:   storefront/public/images/logo-munchees.svg 	modified:   storefront/public/images/logo.svg 	new file:   storefront/public/images/mini-logo-munchies.png 	modified:   storefront/public/images/mini-logo.png

--- backend/src/api/store/email/_templates/components/footer.tsx
+++ backend/src/api/store/email/_templates/components/footer.tsx
@@ -64,7 +64,7 @@ export default function Footer() {
 
         <Section className="text-center">
           <Text style={footer} className="pb-5">
-            © {year} MUNCHIES
+            © {year} MUATTAR STORE
           </Text>
         </Section>
       </Tailwind>
