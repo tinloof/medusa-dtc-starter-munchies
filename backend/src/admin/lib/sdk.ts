bb11e7ed2b37b04f02df997297dfbc92a12766f1
Ehtisham Afzal <khaj3172@gmail.com>
2024-12-21 21:35:57 +0500

    Changes to be committed: 	modified:   backend/src/admin/lib/sdk.ts

--- backend/src/admin/lib/sdk.ts
+++ backend/src/admin/lib/sdk.ts
@@ -1,6 +1,7 @@
 import Medusa from "@medusajs/js-sdk";
 
-export const backendUrl = process.env.BACKEND_URL || "http://localhost:9000";
+export const backendUrl = "https://muattar-store.up.railway.app";
+// export const backendUrl = "http://localhost:9000";
 
 export const sdk = new Medusa({
   baseUrl: backendUrl,
