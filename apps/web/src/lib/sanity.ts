e68278e577daef332746506f8f0f0956f237edec
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2026-01-20 09:14:43 +0100

    fix: fix worker env vars

--- apps/web/src/lib/sanity.ts
+++ apps/web/src/lib/sanity.ts
@@ -3,12 +3,12 @@ import { createClient } from "@sanity/client";
 // Import types to activate module augmentation for automatic type inference
 import "@packages/sanity/types";
 
-export function getClient() {
+export function getClient(env: Env) {
   const client = createClient({
-    projectId: import.meta.env.SANITY_STUDIO_PROJECT_ID,
-    dataset: import.meta.env.SANITY_STUDIO_DATASET,
+    projectId: env.SANITY_STUDIO_PROJECT_ID,
+    dataset: env.SANITY_STUDIO_DATASET,
     apiVersion: "2026-01-16",
-    token: import.meta.env.SANITY_TOKEN,
+    token: env.SANITY_TOKEN,
     useCdn: true,
   });
 
